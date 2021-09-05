320ce132d27ea061699d597260c35b90b8331ccc
Akihiko <yama19980423dada@gmail.com>
2021-09-05 21:57:36 +0900

    front-page 完成

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(function(){
-//今回はcssだけで頑張りました
+    $('.p-header-menu-btn').click(function(){
+        //メニューとbackgroundの表示
+        $('.l-sidebar').addClass('open');
+        $('.c-tablet-background--gray').addClass('open');
+    });
 
-
-
-    
-    
-    
+    $('.p-close-btn').click(function(){
+        //メニューとbackgroundの非表示
+        $('.l-sidebar').removeClass('open');
+        $('.c-tablet-background--gray').removeClass('open');
+    });    
 });
