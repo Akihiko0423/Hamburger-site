0e5d06347043208fc983d765e63eb4b947f95e06
Akihiko <yama19980423dada@gmail.com>
2021-09-06 12:18:38 +0900

    front-page修正

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 $(function(){
     $('.p-header-menu-btn').click(function(){
-        //メニューとbackgroundの表示
-        $('.l-sidebar').addClass('open');
-        $('.c-tablet-background--gray').addClass('open');
+        $('.l-sidebar').addClass('open-slide'); //サイドバーのスライド
+        $('.c-tablet-background--gray').addClass('open');//グレー背景の表示
     });
 
     $('.p-close-btn').click(function(){
-        //メニューとbackgroundの非表示
-        $('.l-sidebar').removeClass('open');
-        $('.c-tablet-background--gray').removeClass('open');
+        $('.l-sidebar').removeClass('open-slide');//サイドバーのスライド
+        $('.c-tablet-background--gray').removeClass('open');//グレー背景の非表示
     });    
 });
